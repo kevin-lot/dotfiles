347a0b6ecff40b506142e3f3169e2e914389735c
Kevin Lot <mail@kevinlot.me>
2025-12-01 22:47:06 +0100

    add Cursor/Vscode

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-cd ./config
-for LN_PATH in ./*; do
-    if [[ -L ~/.config/${LN_PATH} ]]; then continue; fi
-    ln -s ~/.dotfiles/config/${LN_PATH} ~/.config/${LN_PATH}
-done
+ln -sf ~/.dotfiles/config/btop ~/.config/.
+ln -sf ~/.dotfiles/config/fontconfig ~/.config/.
+ln -sf ~/.dotfiles/config/lazygit ~/.config/.
+ln -sf ~/.dotfiles/config/lsd ~/.config/.
+ln -sf ~/.dotfiles/config/micro ~/.config/.
+ln -sf ~/.dotfiles/config/niri ~/.config/.
+ln -sf ~/.dotfiles/config/niriswitcher ~/.config/.
+ln -sf ~/.dotfiles/config/noctalia ~/.config/.
+ln -sf ~/.dotfiles/config/terminator ~/.config/.
+ln -sf ~/.dotfiles/config/zsh ~/.config/.
+
+ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Cursor/User/.
+ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Cursor/User/.
+ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Code/User/.
+ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Code/User/.
+ln -sf ~/.dotfiles/config/starship.toml ~/.config/.
