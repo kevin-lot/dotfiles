5049a2dd7751dbd0503b7667bcf04bfe7b02f260
Kevin Lot <mail+github@kevinlot.me>
2025-12-15 17:16:27 +0100

    update for macos

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-ln -sf ~/.dotfiles/config/btop ~/.config/.
+#set -o xtrace -o errexit
+set -o errexit
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [[ "${OS}" == "linux" ]]; then
+    OS=$(uname -n | tr '[:upper:]' '[:lower:]')
+fi
+
 ln -sf ~/.dotfiles/config/fontconfig ~/.config/.
 ln -sf ~/.dotfiles/config/lazygit ~/.config/.
 ln -sf ~/.dotfiles/config/lsd ~/.config/.
@@ -10,9 +17,16 @@ ln -sf ~/.dotfiles/config/niriswitcher ~/.config/.
 ln -sf ~/.dotfiles/config/noctalia ~/.config/.
 ln -sf ~/.dotfiles/config/terminator ~/.config/.
 ln -sf ~/.dotfiles/config/zsh ~/.config/.
-
-ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Cursor/User/.
-ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Cursor/User/.
-ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Code/User/.
-ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Code/User/.
 ln -sf ~/.dotfiles/config/starship.toml ~/.config/.
+
+if [[ "${OS}" == "fedora" ]]; then
+    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Cursor/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Cursor/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Code/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Code/User/.
+else
+    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/Library/Application\ Support/Cursor/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/Library/Application\ Support/Cursor/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/Library/Application\ Support/Cursor/User/.
+    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/Library/Application\ Support/Cursor/User/.
+fi
