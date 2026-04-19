b69af1ac3a968cd8a65550f1bd9af4d3299c2cd6
Kevin Lot <mail@kevinlot.me>
2026-04-19 17:21:56 +0200

    remove cursor intall

--- install.sh
+++ install.sh
@@ -18,15 +18,3 @@ ln -sf ~/.dotfiles/config/noctalia ~/.config/.
 ln -sf ~/.dotfiles/config/terminator ~/.config/.
 ln -sf ~/.dotfiles/config/zsh ~/.config/.
 ln -sf ~/.dotfiles/config/starship.toml ~/.config/.
-
-if [[ "${OS}" == "fedora" ]]; then
-    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Cursor/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Cursor/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/.config/Code/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/.config/Code/User/.
-else
-    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/Library/Application\ Support/Cursor/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/Library/Application\ Support/Cursor/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/keybindings.json ~/Library/Application\ Support/Cursor/User/.
-    ln -sf ~/.dotfiles/config/Cursor/User/settings.json ~/Library/Application\ Support/Cursor/User/.
-fi
